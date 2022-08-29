ae190f35eca21de15e75895f483076c755850f33
João Victor <joao.victors1203@gmail.com>
2022-08-29 19:22:48 -0300

    add scene and mov

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,7 +6,7 @@ public class PlayerScript : MonoBehaviour
 {
 
     private Rigidbody2D rb2D;
-    private int speed = 600;
+    private int speed = 15;
     private bool jumping = false;
 
     // Start is called before the first frame update
@@ -18,17 +18,21 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKey(KeyCode.A)) {
-        //     // rb2D.MovePosition(rb2D.position + (Vector2.left * speed * Time.deltaTime) );
-        // }
+        jumping = !rb2D.IsTouchingLayers();
 
-        // if (Input.GetKey(KeyCode.D)) {
-        //     // rb2D.MovePosition(rb2D.position + (Vector2.right * speed * Time.deltaTime) );
-        // }
+        if (Input.GetKey(KeyCode.A)) {
+            // rb2D.MovePosition(rb2D.position + (Vector2.left * speed * Time.deltaTime) );
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
 
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.D)) {
+            // rb2D.MovePosition(rb2D.position + (Vector2.right * speed * Time.deltaTime) );
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && !jumping) {
             jumping = true;
-            rb2D.AddForce(Vector2.up * 300);
+            rb2D.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
         }
     }
 }
