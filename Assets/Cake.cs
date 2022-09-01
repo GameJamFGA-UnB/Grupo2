d972c4df12ad182631c7ed0d57c766489d6548ca
Rodrigo Balbino <rodrigo080599@hotmail.com>
2022-09-01 10:41:18 -0300

    arrumando cake

--- Assets/Cake.cs
+++ Assets/Cake.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Cake : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    float initialPos;
     void Start()
     {
-        
+        initialPos = transform.position.x ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x > 28 || transform.position.x < -28) {
+        if (Mathf.Abs(initialPos - transform.position.x ) > 10) {
             Destroy(gameObject);
         }
     }
